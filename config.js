b392a5e5c92be7ad771937cbd9ad00d4d882dffb
shriyans665 <73469664+shriyans665@users.noreply.github.com>
2021-06-19 13:53:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+ import * as firebase from "firebase";
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyAaVCkqsKga0NQziVNKubUMNT23HL6l-50",
+    authDomain: "procject-60.firebaseapp.com",
+    databaseURL: "https://procject-60-default-rtdb.firebaseio.com",
+    projectId: "procject-60",
+    storageBucket: "procject-60.appspot.com",
+    messagingSenderId: "32009003520",
+    appId: "1:32009003520:web:c7bd893b14c14707b86ee5"
+  };
+  // Initialize Firebase
+
+   export default firebase.initializeApp(firebaseConfig);
+ 
+ 
+
+  
